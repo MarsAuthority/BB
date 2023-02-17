fafe8751e7bf77e034046fdb360dfebc3737427e
MarsAuthority <chinamarsmail@gmail.com>
2023-02-17 06:39:23 +0000

    Deploying to gh-pages from @ MarsAuthority/marsauthority.github.io@9d61b1f6a5b8d4aadf739a5acb13d955c0a8ea94 🚀

---
+++ assets/pageview-7db859bb.js
@@ -0,0 +1,1 @@
+import{i as t}from"./app-4d12bbd4.js";import"./framework-fca3d3a7.js";const n=()=>t({serverURL:"https://waline.mars.run"});export{n as updatePageview};
--- assets/pageview-bf251ff0.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./app-f6821c3e.js";import"./framework-fca3d3a7.js";const n=()=>t({serverURL:"https://waline.mars.run"});export{n as updatePageview};
