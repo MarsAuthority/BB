35c23113eb68a215cbcc22021eeaa583799f5c7a
MarsAuthority <chinamarsmail@gmail.com>
2023-02-22 01:04:17 +0800

    add

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -40,9 +40,9 @@ export default defineUserConfig({
     // algolia 全文搜索：没设置爬虫的话，需删除 docsearchPlugin 区块以使用节点搜索
 
     docsearchPlugin({
-      indexName: "marsrun",
-      appId: "I2JZ4WA8D8",
-      apiKey: "e25acd85f89b8f16cb1220e0bf111b1e",
+      indexName: "mars",
+      appId: "K0BYUXJBJR",
+      apiKey: "6477c452b08c931972ae715f46ad4f10",
     }),
     // 本地搜索，删除上方 docsearchPlugin 区块后生效。
     //searchProPlugin({
